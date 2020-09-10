830fbe89f17ea032d6597bfeb5d26ce139f65c5b
muraoka <taigamuraoka1998@gmail.com>
2020-09-10 15:00:54 +0900

    Add exclude parameter to /recipes

--- server/app/controllers/api/recipes_controller.rb
+++ server/app/controllers/api/recipes_controller.rb
@@ -4,6 +4,7 @@ class Api::RecipesController < ApplicationController
   def index
     recipes = Recipe.all
     recipes = recipes.where(time: ..params[:time_max]) if params[:time_max]
+    recipes = recipes.where.not(id: params[:exclude]) if params[:exclude]
 
     @recipes = recipes
     render json: @recipes
